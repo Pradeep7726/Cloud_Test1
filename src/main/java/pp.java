91cf4e6bcce773b55e83b0dbde6627710c8052f4
Pradeep7726 <pradeeplakkineni7@gmail.com>
2023-10-12 11:33:34 +0530

    commit push

--- src/main/java/pp.java
+++ src/main/java/pp.java
@@ -1,9 +1,7 @@
 public class pp {
 	public String s;
 	public static void main(String[] args) {
-		public String p;
 		// TODO Auto-generated method stub
-		System.out.println(a);
 		System.out.println("ok");
 	}
 
